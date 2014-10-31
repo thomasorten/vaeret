c50ddc34359af2f39ea297268cab43542bc38b62
thomasorten <thomas.orten@gmail.com>
2014-10-31 11:20:43 +0100

    Fixed auto layout issues.

--- Vaeret/ViewController.m
+++ Vaeret/ViewController.m
@@ -8,12 +8,14 @@
 
 #import "ViewController.h"
 #import "WeatherCollectionViewCell.h"
+#import "Masonry.h"
 
 #define defaultSearchString @"Søk etter sted"
 
 @interface ViewController () <UITextViewDelegate, UICollectionViewDataSource, UICollectionViewDelegate>
 
 @property(assign) NSInteger currentTaskId;
+
 @property (weak, nonatomic) IBOutlet UIView *searchPlaceView;
 @property (weak, nonatomic) IBOutlet UIView *findMeView;
 @property (weak, nonatomic) IBOutlet UIView *underlineView;
@@ -32,13 +34,15 @@
 @property (weak, nonatomic) IBOutlet UILabel *weatherGridFiveLabel;
 @property (weak, nonatomic) IBOutlet UILabel *weatherGridSixLabel;
 
-@property int initialScrollOffsetPosition;
-@property int initialFindMeScrollOffsetPosition;
-@property int initialSearchViewYPosition;
-@property int initialSearchViewXPosition;
-@property int initialFindMeViewYPosition;
-@property int initialCancelButtonXPosition;
 @property CGRect initialTextFieldFrame;
+
+@property (nonatomic, strong) MASConstraint *logoCenterYConstraint;
+@property (nonatomic, strong) MASConstraint *searchCenterYConstraint;
+@property (nonatomic, strong) MASConstraint *searchCenterXConstraint;
+@property (nonatomic, strong) MASConstraint *findMeCenterYConstraint;
+
+@property float screenHeight;
+@property float screenWidth;
 
 @end
 
@@ -49,12 +53,42 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
+    self.screenWidth = [[UIScreen mainScreen] bounds].size.width;
+    self.screenHeight = [[UIScreen mainScreen] bounds].size.height;
+
     [self.weatherGridOneLabel setText:@"\uf002"];
     [self.weatherGridTwoLabel setText:@"\uf002"];
     [self.weatherGridThreeLabel setText:@"\uf002"];
     [self.weatherGridFourLabel setText:@"\uf002"];
     [self.weatherGridFiveLabel setText:@"\uf002"];
     [self.weatherGridSixLabel setText:@"\uf002"];
+
+
+    // Constraints
+    [self.titleLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.equalTo(self.view);
+        self.logoCenterYConstraint = make.centerY.equalTo(self.view);
+    }];
+    [self.searchPlaceView mas_makeConstraints:^(MASConstraintMaker *make) {
+        self.searchCenterXConstraint = make.centerX.equalTo(self.view);
+        self.searchCenterYConstraint = make.centerY.equalTo(self.view);
+    }];
+    [self.findMeView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.equalTo(self.view);
+        self.findMeCenterYConstraint = make.top.equalTo(self.searchPlaceView.mas_bottom);
+    }];
+    [self.copyrightLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.equalTo(self.view);
+        make.bottom.equalTo(self.view.mas_bottom).with.offset(-30);
+    }];
+    [self.weatherGridView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.equalTo(self.view);
+        make.centerY.equalTo(self.view).centerOffset(CGPointMake(0, self.weatherGridView.frame.size.height));
+    }];
+    [self.weatherCollectionView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.equalTo(self.view);
+        make.centerY.equalTo(self.view).centerOffset(CGPointMake(0, -70));
+    }];
 
     [self generateData];
 
@@ -70,35 +104,15 @@
     
     [self.view addGestureRecognizer:tap];
 
-    self.initialSearchViewYPosition = self.searchPlaceView.frame.origin.y;
-    self.initialSearchViewXPosition = self.searchPlaceView.frame.origin.x;
-    self.initialFindMeViewYPosition = self.findMeView.frame.origin.y;
-    self.initialCancelButtonXPosition = self.cancelButton.frame.origin.x;
-    self.initialTextFieldFrame = self.placeTextField.frame;
-
     [self performSelector:@selector(animateLogo) withObject:nil afterDelay:0.5];
 }
 
 -(void)animateLogo
 {
-    [self.titleLabel.constraints enumerateObjectsUsingBlock:^(NSLayoutConstraint *constraint, NSUInteger idx, BOOL *stop) {
-        if ((constraint.firstItem == self.titleLabel) &&   (constraint.firstAttribute == NSLayoutAttributeCenterY)) {
-            constraint.constant = 48.0;
-        }
-    }];
-
-    [UIView animateWithDuration:0.5 animations:^{
+    self.logoCenterYConstraint.centerOffset(CGPointMake(0, -self.screenHeight*0.35));
+    [UIView animateWithDuration:0.6 animations:^{
         [self.view layoutIfNeeded];
     }];
-
-
-//    [UIView animateWithDuration:0.6 animations:^{
-//        CGRect frame;
-//        // move our subView to its new position
-//        frame=self.titleLabel.frame;
-//        frame.origin.y = 48;
-//        self.titleLabel.frame=frame;
-//    }];
 
     [self performSelector:@selector(fadeInSearch) withObject:nil afterDelay:0.5];
 }
@@ -138,56 +152,19 @@
 // Called when the UIKeyboardDidShowNotification is sent.
 - (void)keyboardWasShown:(NSNotification*)aNotification
 {
-    NSDictionary* info = [aNotification userInfo];
-    CGSize kbSize = [[info objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].size;
-
-    CGRect fieldFrame = self.searchPlaceView.frame;
-    CGRect aRect = self.view.frame;
-    aRect.size.height -= kbSize.height;
-    CGPoint origin = fieldFrame.origin;
-    self.initialScrollOffsetPosition = origin.y;
-
-    CGRect findMeFrame = self.findMeView.frame;
-    self.initialFindMeScrollOffsetPosition = findMeFrame.origin.y;
-
-    origin.y -= (self.searchPlaceView.frame.origin.y+50);
-    if (!CGRectContainsPoint(aRect, origin) ) {
-        [UIView animateWithDuration:0.2 animations:^{
-            CGRect frame;
-            // move our subView to its new position
-            frame = self.searchPlaceView.frame;
-            frame.origin.y = (fieldFrame.origin.y+240)-(aRect.size.height);
-
-            self.searchPlaceView.frame=frame;
-
-            CGRect frame2;
-            // move our subView to its new position
-            frame2 = self.findMeView.frame;
-            frame2.origin.y = (findMeFrame.origin.y)+(aRect.size.height);
-
-            self.findMeView.frame=frame2;
-
-            // Place popover according to new position
-            [self.placeTextField setPopoverSize:CGRectMake(self.searchPlaceView.frame.origin.x, (self.searchPlaceView.frame.origin.y+self.placeTextField.frame.size.height), self.underlineView.frame.size.width, 135)];
-        }];
-    }
+    self.searchCenterYConstraint.centerOffset(CGPointMake(0, -self.screenHeight*0.15));
+    [UIView animateWithDuration:0.2 animations:^{
+        [self.view layoutIfNeeded];
+    }];
+    [self.placeTextField setPopoverSize:CGRectMake(self.searchPlaceView.frame.origin.x, (self.searchPlaceView.frame.origin.y+self.placeTextField.frame.size.height), self.underlineView.frame.size.width, 135)];
 }
 
 // Called when the UIKeyboardWillHideNotification is sent
 - (void)keyboardWillBeHidden:(NSNotification*)aNotification
 {
+    self.searchCenterYConstraint.centerOffset = (CGPointMake(0, 0));
     [UIView animateWithDuration:0.2 animations:^{
-        CGRect frame;
-        // move our subView to its new position
-        frame=self.searchPlaceView.frame;
-        frame.origin.y = self.initialScrollOffsetPosition;
-        self.searchPlaceView.frame=frame;
-
-        CGRect frame2;
-        // move our subView to its new position
-        frame2=self.findMeView.frame;
-        frame2.origin.y = self.initialFindMeScrollOffsetPosition;
-        self.findMeView.frame=frame2;
+        [self.view layoutIfNeeded];
     }];
 }
 
@@ -291,51 +268,44 @@
 
 -(void)animateTitles:(BOOL)reverse
 {
-    int searchViewYPosition = self.initialSearchViewYPosition;
-    int searchViewXPosition = self.initialSearchViewXPosition;
-    int findMeYPosition = self.initialFindMeViewYPosition;
-    int cancelButtonXPosition = self.initialCancelButtonXPosition;
-
-    if (!reverse) {
-        searchViewYPosition = 50;
-        findMeYPosition = [[UIScreen mainScreen] bounds].size.height+50;
-    }
-
     [UIView animateWithDuration:0.3 animations:^{
         self.underlineView.alpha = reverse ? 1 : 0;
         self.titleLabel.alpha = reverse ? 1 : 0;
+        self.findMeView.alpha = reverse ? 1 : 0;
     }];
 
     [UIView animateWithDuration:0.6 animations:^{
 
+        int initialWidth = self.placeTextField.frame.size.width;
+
         [self.placeTextField sizeToFit];
         [self.placeTextField setEnabled:reverse];
 
-        CGRect frame;
-        // move our subView to its new position
-        frame = self.searchPlaceView.frame;
-        frame.origin.y = searchViewYPosition;
-        frame.origin.x = reverse ? searchViewXPosition : (([[UIScreen mainScreen] bounds].size.width-self.placeTextField.frame.size.width)/2)-17;
-        self.searchPlaceView.frame=frame;
+        int newWidth = self.placeTextField.frame.size.width;
 
-        CGRect frame2;
-        // move our subView to its new position
-        frame2 = self.findMeView.frame;
-        frame2.origin.y = findMeYPosition;
-        self.findMeView.frame=frame2;
+        if (reverse) {
+            self.searchCenterYConstraint.centerOffset(CGPointMake(0, 0));
+            self.searchCenterXConstraint.centerOffset(CGPointMake(0, 0));
+        } else {
+            self.searchCenterYConstraint.centerOffset(CGPointMake(0, -self.screenHeight*0.38));
+            self.searchCenterXConstraint.centerOffset(CGPointMake(((initialWidth-newWidth)/2)-17, 0));
+        }
 
-        CGRect frame3;
-        // move our subView to its new position
-        frame3 = self.cancelButton.frame;
-        frame3.origin.x = reverse ? cancelButtonXPosition : (([[UIScreen mainScreen] bounds].size.width+self.placeTextField.frame.size.width)/2);
-        self.cancelButton.frame=frame3;
+        [self.cancelButton mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.mas_equalTo(58);
+            make.left.mas_equalTo(((self.screenWidth+newWidth)/2)-5);
+        }];
 
         self.cancelButton.alpha = reverse ? 0 : 1;
+
+        [UIView animateWithDuration:0.2 animations:^{
+            [self.view layoutIfNeeded];
+        }];
+
     }];
 
     if (reverse) {
-        self.placeTextField.frame= self.initialTextFieldFrame;
-        [self performSelector:@selector(resetTextField) withObject:nil afterDelay:0.6];
+        [self performSelector:@selector(resetTextField) withObject:nil afterDelay:0.3];
         [self performSelector:@selector(animateWeatherViews:) withObject:[NSNumber numberWithBool:YES] afterDelay:0];
     } else {
         [self performSelector:@selector(animateWeatherViews:) withObject:[NSNumber numberWithBool:NO] afterDelay:0.8];
